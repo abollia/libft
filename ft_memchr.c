77787e0d91092b690bc2b3e0afb6562a169bf66f
abollia <arthur.bollia@gmail.com>
2024-11-19 12:30:10 +0100

    Add files via upload

---
+++ ft_memchr.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abollia <abollia@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/18 16:10:18 by abollia           #+#    #+#             */
+/*   Updated: 2024/10/22 10:44:51 by abollia          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	size_t			i;
+	unsigned char	*str;
+	unsigned char	ch;
+
+	i = 0;
+	str = (unsigned char *) s;
+	ch = (unsigned char) c;
+	while (i < n)
+	{
+		if (str[i] == ch)
+			return ((void *)&str[i]);
+		i++;
+	}
+	return (NULL);
+}
---
+++ ft_strdup.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abollia <abollia@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/23 12:05:00 by abollia           #+#    #+#             */
+/*   Updated: 2024/10/23 13:21:42 by abollia          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strdup(const char *s)
+{
+	char	*copy;
+	size_t	i;
+	size_t	len;
+
+	len = ft_strlen(s);
+	copy = (char *) malloc(sizeof(char) * (len + 1));
+	if (copy == NULL)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		copy[i] = s[i];
+		i++;
+	}
+	copy[i] = '\0';
+	return (copy);
+}
